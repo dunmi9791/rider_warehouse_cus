58c021ec16d1c0672e3140a0b44528e636c8b253
dunmi9791 <dominic.anyanna@gmail.com>
2024-06-26 02:04:33 +0100

    first pull data

--- models/get_temp_data.py
+++ models/get_temp_data.py
@@ -11,7 +11,7 @@ class SensorDataImport(models.Model):
     @api.model
     def fetch_and_import_data(self):
         logger = logging.getLogger(__name__)
-        token = 'VXNlci0yNTA3O2Q0MmFlNGViNzI3OTQ2MzU4ZmExN2ZlZDQwMzAxOTRk'
+        token = 'VXNlci0yNTA3OzM4MTFiZDUxOWQ1NjRkOTc4OTg0YjYyYmZiMWY5MTc1'
         # auth_url = 'https://i.cloud.tzonedigital.cn/Identity'
         # auth_params = {
         #     'appId': '5c1169a48591411eac78dc528155f40e',
@@ -51,28 +51,32 @@ class SensorDataImport(models.Model):
 
             data = response.json()
             logger.info(f'Response for device {device_id}: {data}')
-            sensor_data_model = self.env['sensor.data']
-            temperature_device_model = self.env['temperature.device']
+            body = data.get('body')
+            if body and isinstance(body, dict):
+                data_list = body.get('dataList', [])
+                if isinstance(data_list, list):
+                    sensor_data_model = self.env['sensor.data']
+                    temperature_device_model = self.env['temperature.device']
 
-            for item in data.get('body', {}).get('dataList', []):
-                device_record = temperature_device_model.search([('serial_no', '=', device_id)], limit=1)
-                if device_record:
-                    sensor_data_model.create({
-                        'device_id': device_record.id,
-                        'temperature': item.get('temperature'),
-                        'temperature1': item.get('temperature1'),
-                        'humidity': item.get('humidity'),
-                        'humidity1': item.get('humidity1'),
-                        'light': item.get('light'),
-                        'vibration': item.get('vibration'),
-                        'voltage': item.get('voltage'),
-                        'battery': item.get('battery'),
-                        'rssi': item.get('rssi'),
-                        'lat_lng': item.get('latLng'),
-                        'base_station': item.get('baseStation'),
-                        'io': item.get('io'),
-                        'rtc': item.get('rtc'),
-                        'create_time': item.get('createTime'),
-                    })
+                    for item in data_list:
+                        device_record = temperature_device_model.search([('serial_no', '=', device_id)], limit=1)
+                        if device_record:
+                            sensor_data_model.create({
+                                'device_id': device_record.id,
+                                'temperature': item.get('temperature'),
+                                'temperature1': item.get('temperature1'),
+                                'humidity': item.get('humidity'),
+                                'humidity1': item.get('humidity1'),
+                                'light': item.get('light'),
+                                'vibration': item.get('vibration'),
+                                'voltage': item.get('voltage'),
+                                'battery': item.get('battery'),
+                                'rssi': item.get('rssi'),
+                                'lat_lng': item.get('latLng'),
+                                'base_station': item.get('baseStation'),
+                                'io': item.get('io'),
+                                'rtc': item.get('rtc'),
+                                'create_time': item.get('createTime'),
+                            })
 
         logger.info(f'Finished data fetch at {datetime.utcnow()}')
